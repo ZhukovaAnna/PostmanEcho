5113650f455436cc0d3ee1f35b0687c3ce273f1c
ZhukovaAnna <zhukova.anna.home@gmail.com>
2020-06-05 17:29:55 +0300

    Test

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestPostInquiry.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class MobileBankApiTestPostInquiry {
+
+    @Test
+    void shouldPostInquiryPut(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Иванов") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data" , equalTo("Иванов"))
+                ;
+    }
+}
